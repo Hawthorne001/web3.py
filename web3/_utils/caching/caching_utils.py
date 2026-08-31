8552b49a0d94f7ee41c810549ccc932a0a802110
David Dzhalaev <72649244+dzhalaevd@users.noreply.github.com>
2026-08-31 18:00:40 +0300

    feat(eth): remove deprecated `get_uncle_*` method (#3806)
    Co-authored-by: El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>

--- web3/_utils/caching/caching_utils.py
+++ web3/_utils/caching/caching_utils.py
@@ -152,8 +152,6 @@ BLOCKNUM_IN_PARAMS = {
     RPC.eth_getBlockByNumber,
     RPC.eth_getRawTransactionByBlockNumberAndIndex,
     RPC.eth_getBlockTransactionCountByNumber,
-    RPC.eth_getUncleByBlockNumberAndIndex,
-    RPC.eth_getUncleCountByBlockNumber,
 }
 BLOCK_IN_RESULT = {
     RPC.eth_getBlockByHash,
@@ -164,8 +162,6 @@ BLOCK_IN_RESULT = {
 }
 BLOCKHASH_IN_PARAMS = {
     RPC.eth_getRawTransactionByBlockHashAndIndex,
-    RPC.eth_getUncleByBlockHashAndIndex,
-    RPC.eth_getUncleCountByBlockHash,
 }
 
 INTERNAL_VALIDATION_MAP: dict[
